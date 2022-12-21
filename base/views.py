07ba9982d4c8d697c790afe6d4043e0053c695ab
mrjasonwalton00 <jpwalton00@gmail.com>
2022-12-20 22:30:30 -0500

    first

---
+++ base/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request, 'base/home.html')
+
+def page1(request):
+    return render(request, 'base/page1.html')
+
+def page2(request):
+    return render(request, 'base/page2.html')
+
+def page3(request):
+    return render(request, 'base/page3.html')
